598edc0277509aef9811afae5586eea9c0aa7b6b
Abhik Mitra <abhikmitra@fb.com>
2021-05-25 14:44:11 +0100

    Script for checking passport slots

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 var beep = require('beepbeep')
 const notifier = require('node-notifier');
 const path = require('path');
+const ChromeLauncher = require('chrome-launcher');
 // request({
 //     method: 'GET',
 //     url: 'https://www.hcilondon.gov.in/appointment.php?month=04&year=2021&apttype=Submission&locationid=4&serviceid=10#dw',
@@ -13,15 +14,27 @@ const path = require('path');
 // }, (err, res, body) => {
 //     console.log(res);
 // });
+var isChromeOpen = false;
+function openChrome() {
+    if (isChromeOpen) {
+        return;
+    }
+    ChromeLauncher.launch({
+        startingUrl: 'https://www.hcilondon.gov.in/appointment.php?month=04&year=2021&apttype=Submission&locationid=4&serviceid=10#dw'
+    })
+    isChromeOpen = true;
+}
 function checkTheWebsite() {
     let browserLink = null;
     puppeteer.launch({ headless: true }).then((browser) => {
         browserLink = browser;
         return browser.newPage();
     }).then((page) => {
+        
         return page.goto('https://www.hcilondon.gov.in/appointment.php?month=04&year=2021&apttype=Submission&locationid=4&serviceid=10#dw', {
             waitUntil: 'networkidle2',
-        }).then(() => {
+        	})
+         .then(() => {
             return page.setViewport({
                 width: 1920,
                 height: 1080,
@@ -40,15 +53,16 @@ function checkTheWebsite() {
             let enabled_dates = $(".a_enable");
             console.log(disabled_dates.length);
             console.log(enabled_dates.length);
-            if (disabled_dates.length !== 38) {
+            if (disabled_dates.length !== 17) {
                 notifier.notify({
-                    title: 'Urgent : Disabled dates less than 38',
+                    title: 'Urgent : Disabled dates less than 35',
                     message: 'Book passport date Right now!',
                     sound: true, // Only Notification Center or Windows Toasters
                     wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
                 });
-                beep(3, 1000);
+                beep(5, 1000);
                 setInterval(checkTheWebsite, 10000)
+                openChrome();
             }
             if (enabled_dates.length !== 0) {
                 notifier.notify({
@@ -57,11 +71,27 @@ function checkTheWebsite() {
                     sound: true, // Only Notification Center or Windows Toasters
                     wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
                 });
-                setInterval(checkTheWebsite, 20000)
+                setInterval(checkTheWebsite, 20000);
+                openChrome();
             }
         })
     }).then(() => {
         browserLink.close();
+    }).catch((e) => {
+        console.log("Errror", e);
+        setTimeout(() => {
+        	checkTheWebsite();
+        }, 0)
+
+        // setInterval(() => {
+        //     notifier.notify({
+        //         title: 'Urgent : Restart the script',
+        //         message: 'Restart the script!',
+        //         sound: true, // Only Notification Center or Windows Toasters
+        //         wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
+        //     });
+        // }, 2000);
     });
 }
-setInterval(checkTheWebsite, 1800000)
+checkTheWebsite();
+setInterval(checkTheWebsite, 1 * 60 * 1000)
